a7d28862aae349b61dae3325d4995b5951f262b7
Juraj Sranko <31003320+jsranko@users.noreply.github.com>
2021-05-26 16:50:40 +0200

    Initial commit

---
+++ src/test/java/de/sranko_informatik/sispringadmin/SiSpringAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sranko_informatik.sispringadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiSpringAdminApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
