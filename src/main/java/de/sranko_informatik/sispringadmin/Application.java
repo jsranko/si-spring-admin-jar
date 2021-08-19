a2d17575fb843f324214b6e02a7476ab6cffe204
Juraj Sranko <31003320+jsranko@users.noreply.github.com>
2021-08-19 17:59:58 +0200

    init

--- src/main/java/de/sranko_informatik/sispringadmin/Application.java
+++ src/main/java/de/sranko_informatik/sispringadmin/Application.java
@@ -18,30 +18,30 @@
 package de.sranko_informatik.sispringadmin;
 
 
-        import org.springframework.boot.SpringApplication;
-        import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-        import org.springframework.boot.builder.SpringApplicationBuilder;
-        import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-        import org.springframework.context.annotation.Configuration;
-        import org.springframework.context.annotation.Profile;
-        import org.springframework.http.HttpMethod;
-        import org.springframework.security.config.Customizer;
-        import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-        import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-        import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-        import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-        import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
-        import de.codecentric.boot.admin.server.config.AdminServerProperties;
-        import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import de.codecentric.boot.admin.server.config.AdminServerProperties;
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
 @Configuration(proxyBeanMethods = false)
 @EnableAutoConfiguration
 @EnableAdminServer
-public class SiSpringAdminApplication extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
-        SpringApplication.run(SiSpringAdminApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
     @Override
